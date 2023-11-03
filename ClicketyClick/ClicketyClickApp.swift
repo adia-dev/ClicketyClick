8a2c65b592a409955f5045e06b8c26386fcd6223
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-11-04 00:18:39 +0100

    Initial Commit

---
+++ ClicketyClick/ClicketyClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClicketyClickApp.swift
+//  ClicketyClick
+//
+//  Created by Abdoulaye Dia on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClicketyClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
